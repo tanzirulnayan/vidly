a13469fc984961378a31460236a33ddc987ebc52
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-02-17 17:25:39 +0600

    Unnecessary files have been deleted

--- vidlyDbContext/Entities/Movie.cs
+++ vidlyDbContext/Entities/Movie.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace vidlyDbContext.Entities
 {
@@ -11,9 +13,13 @@ namespace vidlyDbContext.Entities
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Genre { get; set; }
         public int Year { get; set; }
+        public string ImagePath { get; set; }
         public int BorrowCount { get; set; }
+
+        
     }
 }
